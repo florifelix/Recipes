0881b9c2503d346dbbe633ff4858adb71cbe3bae
floridefix <lenjavoigt24@gmail.com>
2021-12-27 15:52:51 +0100

    checkin

---
+++ app/src/main/java/com/jens/recipes/EierlikörActivity.java
@@ -0,0 +1,29 @@
+package com.jens.recipes;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class EierlikörActivity extends RecipeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView titleView = findViewById(R.id.title);
+        titleView.setText("Eierlikör");
+
+        // Instantiate a ViewPager2 and a PagerAdapter.
+        viewPager = findViewById(R.id.pager);
+        FragmentStateAdapter pagerAdapter = new ScreenSlidePagerAdapter(this,
+                R.drawable.pizzacalzone111,
+                "eierlikoerIngredients.html",
+                "eierlikoerRecipe.html"
+        );
+        viewPager.setAdapter(pagerAdapter);
+        //viewPager.setPageTransformer(new ZoomOutPageTransformer());
+        // TabSlider can only be set after adapter is set to viewpager
+        createTabSlider();
+    }
+}
