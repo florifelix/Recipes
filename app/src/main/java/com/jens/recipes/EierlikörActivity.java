bfc91c3c261d1a2765c84c0f41455d2979f0d333
voigtjens <jensvoigt14@gmail.com>
2021-12-27 16:04:26 +0100

    Test commit

--- app/src/main/java/com/jens/recipes/EierlikörActivity.java
+++ app/src/main/java/com/jens/recipes/EierlikörActivity.java
@@ -17,7 +17,7 @@ public class EierlikörActivity extends RecipeActivity {
         // Instantiate a ViewPager2 and a PagerAdapter.
         viewPager = findViewById(R.id.pager);
         FragmentStateAdapter pagerAdapter = new ScreenSlidePagerAdapter(this,
-                R.drawable.pizzacalzone111,
+                R.drawable.dorade111,
                 "eierlikoerIngredients.html",
                 "eierlikoerRecipe.html"
         );
